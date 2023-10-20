9f1cbfd517076152d3d5ac2f99ba104046ea291a
DavidKirkitadze <davidkirkitadze00@gmail.com>
2023-10-20 13:36:10 +0400

    I added and connected Html file I also changed and removed code *HttpRespons*

--- core/main/views.py
+++ core/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 
 def index(request):
-    return HttpResponse('<h4>check work</h4>')
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse('<h4>this page about us</h4>')
+    return render(request, 'main/about.html')
     
