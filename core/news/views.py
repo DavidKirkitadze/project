563e61a0184b1cb6408def65512167f746544274
DavidKirkitadze <davidkirkitadze00@gmail.com>
2023-11-08 21:30:31 +0400

    update v1.6.2: this is not full update!

--- core/news/views.py
+++ core/news/views.py
@@ -3,12 +3,19 @@ from .models import Articles
 from .forms import ArticlesForm
 from django.views.generic import DetailView, UpdateView, DeleteView
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import ArticlesSerializer
 
 
-class ArticlesAPIView(generics.ListAPIView):
-    queryset = Articles.objects.all()
-    serializer_class = ArticlesSerializer
+
+class ArticlesAPIView(APIView):
+    def get(self, request):
+        return Response({'title': 'Ford RS200'})
+
+#class ArticlesAPIView(generics.ListAPIView):
+    #queryset = Articles.objects.all()
+    #serializer_class = ArticlesSerializer
 
 
 
